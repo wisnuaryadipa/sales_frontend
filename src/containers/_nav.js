1c873ac74596e02e82e01f7375be512ccdbf9a5b
Wisnu <wisnuaryadipa93@gmail.com>
2020-08-18 11:04:30 +0700

    First init

---
+++ src/containers/_nav.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer'
+  },  {
+    _tag: 'CSidebarNavItem',
+    name: 'Visit',
+    to: '/dashboard',
+    icon: 'cil-speedometer'
+  },
+  {
+    _tag: 'CSidebarNavTitle',
+    _children: ['Master Data']
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Customer',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Toko',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'User',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+]
+
